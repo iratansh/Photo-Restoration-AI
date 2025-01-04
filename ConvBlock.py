c1efe76e7b60077ad0f4c4078560a16aa0b07722
Ishaan Ratanshi <i.ratanshi1@gmail.com>
2025-01-04 12:55:09 -0700

    Started ConvBloc

---
+++ ConvBlock.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+    
+    def forward(self, x):
+        return self.conv(x)
